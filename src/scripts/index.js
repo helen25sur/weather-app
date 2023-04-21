ecf42bde8d43a5d22107cd9333b2a7ab2af743e8
Elena Surilova <elen21surilova@gmail.com>
2023-04-21 23:12:55 +0300

    fix: add new information about current humidity & wind speed, add new icons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,35 +13,68 @@ function displayTime() {
   ];
   const date = new Date();
   const dayOfWeek = date.getDay();
-  const hour = date.getHours().length === 1? "0" + date.getHours() : date.getHours();
-  const minute = date.getMinutes().length === 1? "0" + date.getMinutes() : date.getMinutes();
+  const hour = date.getHours().toString().length === 1 ? "0" + date.getHours() : date.getHours();
+  const minute = date.getMinutes().toString().length === 1 ? "0" + date.getMinutes() : date.getMinutes();
 
   const time = `${week[dayOfWeek]}, ${hour}:${minute}`;
   const currentDate = document.body.querySelector("#current-date>strong");
   currentDate.innerText = time;
 }
 
+function addWeatherIcon(cityValue) {
+  const apiKeySheCodes = "be7of017954f4b25219t2327c4fa94c7";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKeySheCodes}&units=metric`;
+  axios
+    .get(`${apiUrl}`)
+    .then((response) => {
+      console.log(response);
+      const currentIcon = document.body.querySelector("#current-temperature-icon");
+      const iconUrl = response.data.condition.icon_url;
+
+      const iconEl = document.createElement("img");
+      iconEl.src = iconUrl;
+      iconEl.classList.add("weather-icon");
+      iconEl.alt = "weather icon";
+      iconEl.style.width = "70px";
+      currentIcon.innerHTML = '';
+      currentIcon.appendChild(iconEl);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 function displayWeather(response) {
   console.log(response);
   const temp = Math.round(response.data.main.temp);
   const cityName = response.data.name;
-  const iconCode = response.data.weather[0].icon;
+  const humidity = response.data.main.humidity;
+  const windSpeed = response.data.wind.speed;
+  // const iconCode = response.data.weather[0].icon;
   city.innerText = cityName;
+
   const currentTemp = document.body.querySelector("#current-temperature");
   const currentDescription = document.body.querySelector("#current-temperature-description");
-  const currentIcon = document.body.querySelector("#current-temperature-icon");
-  const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-  const iconEl = document.createElement("img");
-  iconEl.src = iconUrl;
-  iconEl.classList.add("weather-icon");
-  iconEl.alt = "weather icon";
-  iconEl.style.width = "70px";
+  // const currentIcon = document.body.querySelector("#current-temperature-icon");
+  const currentHumidity = document.body.querySelector("#humidity>span");
+  const currentWindSpeed = document.body.querySelector("#wind-speed>span");
 
-  console.log(iconCode, iconUrl);
+  // const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+  // const iconEl = document.createElement("img");
+  // iconEl.src = iconUrl;
+  // iconEl.classList.add("weather-icon");
+  // iconEl.alt = "weather icon";
+  // iconEl.style.width = "60px";
+
+  // console.log(iconCode, iconUrl);
   currentTemp.innerText = temp;
-  currentDescription.innerText = response.data.weather[0].main;
-  currentIcon.innerHTML = '';
-  currentIcon.appendChild(iconEl);
+  currentDescription.innerText = response.data.weather[0].description;
+  // currentIcon.innerHTML = '';
+  // currentIcon.appendChild(iconEl);
+
+  currentHumidity.innerText = humidity;
+  currentWindSpeed.innerText = windSpeed;
+
   console.log(currentTemp, temp);
 }
 
@@ -61,6 +94,7 @@ function searchCity(event) {
   const searchInput = document.body.querySelector('#search');
   if (searchInput.value) {
     getWeather(searchInput.value.trim());
+    addWeatherIcon(searchInput.value.trim());
   }
   searchInput.value = "";
 }
@@ -119,3 +153,4 @@ displayTime();
 setInterval(displayTime, 60000);
 
 getWeather(city.innerText, city);
+addWeatherIcon(city.innerText);
