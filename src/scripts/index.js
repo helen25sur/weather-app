bc204d8e6e7c967ad331dbd831af125ccd103ea7
Elena Surilova <elen21surilova@gmail.com>
2023-04-29 00:56:01 +0300

    fix: change default api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,13 +14,13 @@ function displayTime() {
 }
 
 function displayWeather(response) {
-  // console.log(response);
-  const temp = Math.round(response.data.main.temp);
-  const cityName = response.data.name;
-  const humidity = response.data.main.humidity;
+  console.log(response);
+  const temp = Math.round(response.data.temperature.current);
+  const cityName = response.data.city;
+  const humidity = response.data.temperature.humidity;
   const windSpeed = response.data.wind.speed;
-  const iconCode = response.data.weather[0].icon;
-  city.innerText = cityName;
+  const iconUrl = response.data.condition.icon_url;
+  const description = response.data.condition.description;
 
   const currentTemp = document.body.querySelector("#current-temperature");
   const currentDescription = document.body.querySelector("#current-temperature-description");
@@ -28,28 +28,28 @@ function displayWeather(response) {
   const currentHumidity = document.body.querySelector("#humidity>span");
   const currentWindSpeed = document.body.querySelector("#wind-speed>span");
 
-  const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  // const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
   const iconEl = document.createElement("img");
   iconEl.src = iconUrl;
   iconEl.classList.add("weather-icon");
-  iconEl.alt = "weather icon";
+  iconEl.alt = `${response.data.condition.icon} icon`;
   iconEl.style.width = "60px";
 
-  // console.log(iconCode, iconUrl);
+  const city = document.body.querySelector('#city');
+  city.innerText = cityName;
   currentTemp.innerText = temp;
-  currentDescription.innerText = response.data.weather[0].description;
+  currentDescription.innerText = description;
   currentIcon.innerHTML = '';
   currentIcon.appendChild(iconEl);
 
   currentHumidity.innerText = humidity;
   currentWindSpeed.innerText = windSpeed;
-
-  // console.log(currentTemp, temp);
 }
 
 function getWeather(cityValue) {
-  const apiKey = "fc1413ef13107061f82b437eba029747";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
+  const apiKey = "be7of017954f4b25219t2327c4fa94c7";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}&units=metric`;
   axios
     .get(`${apiUrl}`)
     .then(displayWeather)
@@ -113,8 +113,8 @@ function showCurrentLocationWeather() {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
-    const apiKey = "fc1413ef13107061f82b437eba029747";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    const apiKey = "be7of017954f4b25219t2327c4fa94c7";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
     
     axios
       .get(`${apiUrl}`)
@@ -160,6 +160,6 @@ locationBtn.addEventListener('click', showCurrentLocationWeather);
 displayTime();
 setInterval(displayTime, 60000);
 
-getWeather(city.innerText, city);
+getWeather(city.innerText);
 
 getForecast(city.innerText);
